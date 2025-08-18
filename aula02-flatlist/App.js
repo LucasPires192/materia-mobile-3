d430fe4f7a978963f00648f7b68736835dadecf3
calegermano <carlosalexandre071221@gmail.com>
2025-08-18 20:54:44 -0300

    feat: configuracao inicial e criacao da lista

---
+++ aula02-flatlist/App.js
@@ -0,0 +1,44 @@
+export default function App() {
+
+  const animes = [
+    {
+      id: 0,
+      nome: 'Naruto Classico',
+      estilo: 'Shounen',
+      ano: 2002,
+      img: URL('./assets/img/capa-naruto.jpg'),
+    },
+    {
+      id: 1,
+      nome: 'Naruto Shippuden',
+      estilo: 'Shounen',
+      ano: 2007,
+      img: URL('./assets/img/capa-naruto-shippuden.jpg'),
+    },
+    {
+      id: 2,
+      nome: 'Fullmetal Alchemist',
+      estilo: 'Shounen',
+      ano: 2009,
+      img: URL('./assets/img/capa-fullmetal-alchemsit.jpg'),
+    },
+    {
+      id: 3,
+      nome: 'Haikyuu',
+      estilo: 'Esportes',
+      ano: 2014,
+      img: URL('./assets/img/capa-haikyu.jpg'),
+    },
+    {
+      id: 4,
+      nome: 'One Piece',
+      estilo: 'Shounen',
+      ano: 1999,
+      img: URL('./assets/img/capa-one-piece.jpg'),
+    },
+  ]
+  return (
+    <>
+    </>  
+  );
+}
