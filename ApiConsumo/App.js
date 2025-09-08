c446aae6f8ac88e846694d804be1d017c1cf6366
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-08 20:32:22 -0300

    feat: codigo do arquivo App

--- ApiConsumo/App.js
+++ ApiConsumo/App.js
@@ -1,6 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+
+const request = async (callback) => {
+    const response = await fetch('https://swapi.dev/api/people/');
+    const parsed = await response.json();
+    callback(parsed.results);
+}
+
 export default function App() {
+    const [registros, setRegistros] = useState([]);
+
+    useEffect(() => {
+        request(setRegistros);
+    }, [])
+
     return (
-        <>
-        </>
+        <View>
+            <View>
+                <Text>
+                    StarWars
+                </Text>
+            </View>
+
+            <FlatList
+                data={registros}
+                keyExtractor={(item) => item.name.toString()}
+                renderItem={({item}) =>
+                    <Text>
+                        <Text>Nome: {item.name}{'\n'}</Text>
+                        <Text>Peso: {item.mass}{'\n'}</Text>
+                    </Text>
+                }
+            />
+        </View>
     );
 }
